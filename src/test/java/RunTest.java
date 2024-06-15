ca60b87306047a2e3eba6297d0ff0964ecc93f29
ganeshnmblkr <ganeshnmblkr@gmail.com>
2024-06-15 18:34:19 +0530

    Parallel Execution without sureFirePlugin

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -1,4 +1,3 @@
-package StepDefinitions;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
@@ -8,14 +7,10 @@ import org.testng.annotations.DataProvider;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features",
         glue={"StepDefinitions","cucumberHooks"},
-//        tags = "@smoke",
         monochrome = true,
 plugin ={"pretty","html:target/Reports/HtmlReports.html",
 "json:target/Reports/JSONreports.json","junit:target/Reports/XMLreports.xml"})
-//public class TestRunner{
-//
-//}
-public class TestRunner  extends AbstractTestNGCucumberTests {
+public class RunTest  extends AbstractTestNGCucumberTests {
     @Override
     @DataProvider(parallel = true)
         public Object[][] scenarios(){
